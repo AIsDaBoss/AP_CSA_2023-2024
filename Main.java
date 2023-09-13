bf544449f4912ff50d5a4f8859bf19ef21553d9d
Andreas Wieslander <139798604+AIsDaBoss@users.noreply.github.com>
2023-09-13 10:18:34 -0400

    completed 1.3 Expressions and Assignment Statements

--- Main.java
+++ Main.java
@@ -1,17 +1,22 @@
 public class Main
 {
-  public static void main (String[] args)
+  public static void main(String[] args)
   {
+    final int x1 = 3;
+    final double y1 = 65.34;
+    final int x2 = 5;
+    final double y2 = 20.71;
+    double slope = 0;
+    boolean slopeIsPos = false; 
     
-    boolean boolName = true;
-  
-    int intName = 4;
+    System.out.println("The coordinates of the first point are (" + x1 + ", " + y1 + ")");
+    System.out.println("The coordinates of the second point are (" + x2 + ", " + y2 + ")");
     
-    final double DOUBNAME = 12.34;
-  
-    System.out.println("Boolean = " + boolName);
-    System.out.println("Integer = " + intName);
-    System.out.println("Double = " + DOUBNAME);
+    slope = (y2 - y1) / (x2 - x1);
+    System.out.println("The slope between the two points is " + slope);
     
+    slopeIsPos = (slope > 0);
+    System.out.println("Is the slope between the two points positive?: " + slopeIsPos);
   }
 }
+    
