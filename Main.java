dee1f3ac78d680575f75f8e7fc51226375c0fdc6
student <wieslanderandreas@gmail.com>
2023-10-30 12:50:22 -0400

    completed 2.9 Using the Math Class

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
   public static void main(String[] args)
   {
 
-    Calculator c = new Calculator(3); //constructing object
+    Calculator c = new Calculator((int)(Math.random() * 100)); //constructing object + method from Math class
 
     c.intro(); //calling void nonstatic method through object
     c.setCoord2(c.getFavNum()); //calling non-void method as a parameter
